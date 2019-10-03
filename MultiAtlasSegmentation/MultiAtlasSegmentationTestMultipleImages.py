f848114b8379206a0e1a95d21e95ef440f077469
Martin Belzunce <martin.a.belzunce@gmail.com>
2019-10-03 18:52:49 +0100

    To test multiple iamges in python.

--- MultiAtlasSegmentation/MultiAtlasSegmentationTestMultipleImages.py
+++ MultiAtlasSegmentation/MultiAtlasSegmentationTestMultipleImages.py
@@ -3,7 +3,7 @@
 
 from __future__ import print_function
 from GetMetricFromElastixRegistration import GetFinalMetricFromElastixLogFile
-
+from MultiAtlasSegmentation import MultiAtlasSegmentation
 import SimpleITK as sitk
 import numpy as np
 import sys
@@ -11,10 +11,10 @@ import os
 
 ############################### CONFIGURATION #####################################
 DEBUG = 0 # In debug mode, all the intermediate iamges are written.
-USE_COSINES_AND_ORIGIN = 0
+USE_COSINES_AND_ORIGIN = 1
 
 ############################### TARGET FOLDER ###################################
-targetPath = "D:\\Martin\\Segmentation\\AtlasLibrary\\RawImagesTrimmed\\"
+targetPath = "D:\\Martin\\Segmentation\\AtlasLibrary\\V1.0\\NativeResolutionAndSize\\"#""D:\\Martin\\Segmentation\\AtlasLibrary\\V1.0\\NativeResolutionAndSize\\"
 # Look for the raw files in the library:
 files = os.listdir(targetPath)
 extensionImages = 'mhd'
@@ -35,15 +35,15 @@ print("List of files: {0}\n".format(targetImagesNames))
 
 ###################### OUTPUT #####################
 # Output path:
-baseOutputPath = "D:\\MuscleSegmentationEvaluation\\SegmentationWithPython\\FullTest\\"
+baseOutputPath = "D:\\MuscleSegmentationEvaluation\\SegmentationWithPython\\LibraryV1.0_Test\\"
 if not os.path.exists(baseOutputPath):
     os.mkdir(baseOutputPath)
 
 ############################## MULTI-ATLAS SEGMENTATION PARAMETERS ######################
 # Parameter files:
-parameterFilesPath = 'D:\\Martin\\Segmentation\\Registration\\Elastix\\ParametersFile\\'
-paramFileRigid = 'Parameters_Rigid'
-paramFileBspline = 'Parameters_BSpline_NCC'
+#parameterFilesPath = 'D:\\Martin\\Segmentation\\Registration\\Elastix\\ParametersFile\\'
+#paramFileRigid = 'Parameters_Rigid'
+#paramFileBspline = 'Parameters_BSpline_NCC'
 #paramFilesToTest = {'Parameters_BSpline_NCC','Parameters_BSpline_NCC_1000iters', 'Parameters_BSpline_NCC_4096samples', 'Parameters_BSpline_NCC_1000iters_4096samples'}
 
 # Library path:
@@ -72,139 +72,48 @@ for targetFilename in targetImagesNames:
         fixedImage.SetDirection((1, 0, 0, 0, 1, 0, 0, 0, 1))
     path, filename = os.path.split(targetImageFilename)
     nameFixed, extension = os.path.splitext(filename)
+    #nameCaseFixed = nameFixed
     index_dash = nameFixed.index('_')
     nameCaseFixed = nameFixed[:index_dash]
 
     # Output path:
     outputPath = baseOutputPath + nameFixed + "\\"
-    # Temp path:
-    tempPath = outputPath + 'temp' + '\\'
-    if not os.path.exists(outputPath):
-        os.mkdir(outputPath)
-    if not os.path.exists(tempPath):
-        os.mkdir(tempPath)
 
     # Apply bias correction filter:
+    #inputImage = sitk.Shrink(fixedImage, [int(sys.argv[3])] * inputImage.GetDimension())
+    #maskImage = sitk.Shrink(maskImage, [int(sys.argv[3])] * inputImage.GetDimension())
+    #biasFilter = sitk.N4BiasFieldCorrectionImageFilter()
+    #biasFilter.
     #fixedImage = sitk.N4BiasFieldCorrection(fixedImage)
-
-    # If debugging, write image:
-    if DEBUG:
-        sitk.WriteImage(fixedImage, outputPath + "input_registration.mhd")
     ############################################
 
     ############# 1) ATLAS LIBRARY ##############################
-    # Look for the raw files in the library:
+    # To the function I just need to pass the folder name, but first we need to be sure
+    # that the atlas is not in the library folder:
+    backupFolder = libraryPath + "OutOfLibrary\\"
+    if not os.path.exists(backupFolder):
+        os.mkdir(backupFolder)
     files = os.listdir(libraryPath)
     extensionImages = 'mhd'
-    atlasImagesNames = []
-    atlasLabelsNames = []
+    filesToMove = []
     for filename in files:
-        name, extension = os.path.splitext(filename)
-        #    # Use only the marathon study
-        #    if str(name).startswith("ID"):
-        if str(extension).endswith(extensionImages) and not str(name).endswith('labels') and not name.startswith(
-                nameCaseFixed):
-            # Intensity image:
-            atlasImagesNames.append(name + '.' + extensionImages)
-            # Label image:
-            atlasLabelsNames.append(name + '_labels.' + extensionImages)
+        if filename.startswith(nameCaseFixed):
+            # Add to files to move:
+            filesToMove.append(filename)
+    # Move the files for the atlas to the backupfolder:
+    for fileToMove in filesToMove:
+        os.rename(libraryPath + fileToMove, backupFolder + fileToMove)
 
-    print("Number of atlases: {0}".format(len(atlasImagesNames)))
-    print("List of files: {0}\n".format(atlasImagesNames))
-    ##################################################################
+    ########################################################################
+    ################ CALL MULTI ATLAS SEGMENTATION #########################
+    softTissueMask = sitk.Greater(fixedImage, 0)
+    softTissueMask.SetSpacing(fixedImage.GetSpacing())
+    softTissueMask.SetOrigin(fixedImage.GetOrigin())
+    softTissueMask.SetDirection(fixedImage.GetDirection())
+    MultiAtlasSegmentation(fixedImage, softTissueMask, libraryPath, outputPath, DEBUG)
 
-    ############### 2) IMAGE REGISTRATION ###########################
-    # 1) Image registration between atlases and target images:
-    registeredImages = []
-    transformParameterMaps = []
-    similarityValue = []
-    # Register to each atlas:
-    for i in range(0, atlasImagesNames.__len__()):
-        filenameAtlas = atlasImagesNames[i]
-        movingImage = sitk.ReadImage(libraryPath + filenameAtlas)
-        nameMoving, extension = os.path.splitext(filenameAtlas)
-        # elastixImageFilter filter
-        elastixImageFilter = sitk.ElastixImageFilter()
-        # Parameter maps:
-        parameterMapVector = sitk.VectorOfParameterMap()
-        parameterMapVector.append(elastixImageFilter.ReadParameterFile(parameterFilesPath
-                                                                       + paramFileRigid + '.txt'))
-        parameterMapVector.append(elastixImageFilter.ReadParameterFile(parameterFilesPath
-                                                                       + paramFileBspline + '.txt'))
-        # Registration:
-        elastixImageFilter.SetFixedImage(fixedImage)
-        elastixImageFilter.SetMovingImage(movingImage)
-        elastixImageFilter.SetParameterMap(parameterMapVector)
-        elastixImageFilter.LogToConsoleOff()
-        elastixImageFilter.LogToFileOn()
-        elastixImageFilter.SetOutputDirectory(tempPath)
-        logFilename = 'reg_log_{0}'.format(i) + '.txt' # iT DOESN'T WORK WITH DIFFERENT LOG NAMES
-        #logFilename = 'reg_log' + '.txt'
-        elastixImageFilter.SetLogFileName(logFilename)
-        elastixImageFilter.Execute()
-        # Get the images:
-        registeredImages.append(elastixImageFilter.GetResultImage())
-        transformParameterMaps.append(elastixImageFilter.GetTransformParameterMap())
-        # Get the similarity value:
-        fullLogFilename = tempPath + logFilename
-        # Compute normalized cross correlation:
-        imRegMethod = sitk.ImageRegistrationMethod()
-        imRegMethod.SetMetricAsCorrelation()
-        metricValue = imRegMethod.MetricEvaluate(fixedImage, registeredImages[i])
-        # metricValue = sitk.NormalizedCorrelation(registeredImages[i], mask, fixedImage) # Is not working
-        similarityValue.append(metricValue)
-        #similarityValue.append(GetFinalMetricFromElastixLogFile(fullLogFilename))
-        print(similarityValue[i])
-        # If debugging, write image:
-        if DEBUG:
-            outputFilename = outputPath + '\\' + nameMoving + '_to_' + nameFixed + '.mhd'
-            sitk.WriteImage(registeredImages[i], outputFilename)
-
-    ###########################################
-
-    #################### 3) ATLAS SELECTION #################################
-    # convert similarity value in an array:
-    similarityValue = np.asarray(similarityValue)
-    indicesSorted = np.argsort(similarityValue)
-    similarityValuesSorted = similarityValue[indicesSorted]
-    print(similarityValuesSorted)
-    # Selected atlases:
-    indicesSelected = indicesSorted[0:numSelectedAtlases-1]
-    print(indicesSelected)
-    ############################################
-
-    #################### 4) LABEL PROPAGATION #######################
-    propagatedLabels = []
-    for i in range(0, len(indicesSelected)):
-        # Read labels:
-        filenameAtlas = atlasLabelsNames[indicesSelected[i]]
-        labelsImage = sitk.ReadImage(libraryPath + filenameAtlas)
-        nameMoving, extension = os.path.splitext(filenameAtlas)
-        # Apply its transform:
-        transformixImageFilter = sitk.TransformixImageFilter()
-        transformixImageFilter.SetMovingImage(labelsImage)
-        transformixImageFilter.SetTransformParameterMap(transformParameterMaps[indicesSelected[i]])
-        transformixImageFilter.SetTransformParameter("FinalBSplineInterpolationOrder", "0")
-        transformixImageFilter.SetTransformParameter("ResultImagePixelType", "unsigned char")
-        transformixImageFilter.Execute()
-        print(transformixImageFilter.GetTransformParameterMap())
-        propagatedLabels.append(sitk.Cast(transformixImageFilter.GetResultImage(), sitk.sitkUInt8))
-        # If debugging, write label image:
-        if DEBUG:
-            outputFilename = outputPath + '\\' + nameMoving + '_propagated.mhd'
-            sitk.WriteImage(propagatedLabels[i], outputFilename)
-    ###############################################
-
-    ##################### 5) LABEL FUSION #################################
-    outputLabels = sitk.LabelVoting(propagatedLabels, numLabels)
-    multilabelStaple = sitk.MultiLabelSTAPLEImageFilter()
-    multilabelStaple.SetTerminationUpdateThreshold(1e-4)
-    multilabelStaple.SetMaximumNumberOfIterations(30)
-    multilabelStaple.SetLabelForUndecidedPixels(numLabels)
-    outputLabels2 = multilabelStaple.Execute(propagatedLabels)
-    ##############################
-
-    ##################### 6) OUTPUT ############################
-    # Write output:
-    sitk.WriteImage(outputLabels, outputPath + "segmentedImage.mhd")
-    sitk.WriteImage(outputLabels2, outputPath + "segmentedImageSTAPLES.mhd")
+    ########################################################################
+    ##### MOVE BACK FILES
+    # Move the files for the atlas to the backupfolder:
+    for fileToMove in filesToMove:
+        os.rename(backupFolder + fileToMove, libraryPath + fileToMove)
