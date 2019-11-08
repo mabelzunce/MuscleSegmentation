4b8351f83643c73dee7d1ac496a4d78c055cd458
Martin Belzunce <martin.a.belzunce@gmail.com>
2019-11-08 19:20:19 +0000

    New image registration text.

--- ImageRegistration/CompareImageRegistrationMethods.py
+++ ImageRegistration/CompareImageRegistrationMethods.py
@@ -7,15 +7,17 @@ import SimpleITK as sitk
 import numpy as np
 import sys
 import os
+import time as time
 
 # Output path:
-outputPath = "D:\\Martin\\Segmentation\\Registration\\Elastix\\Tests\\"
+outputPath = "D:\\MuscleSegmentationEvaluation\\RegistrationParameters\\"
 # Parameter files:
 parameterFilesPath = 'D:\\Martin\\Segmentation\\Registration\\Elastix\\ParametersFile\\'
-paramFileRigid = 'Parameters_Rigid'
-paramFilesToTest = {'Parameters_BSpline','Parameters_BSpline_NCC'}
+paramFileRigid = 'Parameters_Rigid_NCC'
+paramFilesToTest = ['Parameters_BSpline_NCC', 'Parameters_BSpline_NCC_1000iters_4096samples', 'Parameters_BSpline_NCC_8192samples', 'Parameters_BSpline_NCC_2000iters_8192samples', 'Parameters_BSpline_NCC_4000iters_8192samples']
+paramFilesToTest = ['Parameters_BSpline_NCC_4000iters_8192samples_3levels']
 # Library path:
-libraryPath = "D:\\Martin\\Segmentation\\AtlasLibrary\\V0.6\\Normalized\\"
+libraryPath = "D:\\Martin\\Segmentation\\AtlasLibrary\\V1.0\\Normalized\\"
 # Look for the raw files:
 files = os.listdir(libraryPath)
 extensionImages = 'mhd'
@@ -30,16 +32,29 @@ print("Number of atlases: {0}".format(len(rawImagesNames)))
 print("List of files: {0}\n".format(rawImagesNames))
 
 
+
 #Test all the images with all the parameter sets:
-for filename in rawImagesNames:
+fixedImageNames = [rawImagesNames[4]]
+movingImageNames = [rawImagesNames[5]]
+metricValues = []
+executionTimes = []
+for filename in fixedImageNames:
     fixedImage = sitk.ReadImage(libraryPath + filename)
     nameFixed, extension = os.path.splitext(filename)
-    for filenameMoving in rawImagesNames:
+    # Create an output directory for each parameter file:
+    outputPathThisCase = outputPath + nameFixed + "\\"
+    if not os.path.exists(outputPathThisCase):
+        os.mkdir(outputPathThisCase)
+
+    sitk.WriteImage(fixedImage, outputPathThisCase + 'fixedImage.mhd')
+    for filenameMoving in movingImageNames:
         movingImage = sitk.ReadImage(libraryPath + filenameMoving)
         nameMoving, extension = os.path.splitext(filenameMoving)
         if filename != filenameMoving:
             for paramFiles in paramFilesToTest:
-
+                # Time to compute execution time:
+                startTime = time.time()
+                # The registration
                 # elastixImageFilter filter
                 elastixImageFilter = sitk.ElastixImageFilter()
                 # Parameter maps:
@@ -56,9 +71,20 @@ for filename in rawImagesNames:
                 # Get the images:
                 resultImage = elastixImageFilter.GetResultImage()
                 transformParameterMap = elastixImageFilter.GetTransformParameterMap()
+                # Get metric:
+                imRegMethod = sitk.ImageRegistrationMethod()
+                imRegMethod.SetMetricAsCorrelation()
+                metricValue = imRegMethod.MetricEvaluate(fixedImage, resultImage)
+                print(metricValue)
+                metricValues.append(metricValue)
+                # Stop time
+                stopTime = time.time()
+                # Execution times:
+                executionTimes.append(stopTime-startTime)
                 # Write image:
-                outputFilename = outputPath + nameFixed + '_' + nameMoving + paramFiles + '.mhd'
+                outputFilename = outputPathThisCase + '\\' + nameFixed + '_' + nameMoving + '_' + paramFiles + '.mhd'
                 sitk.WriteImage(resultImage, outputFilename)
 
+print('Similarity values: {0}'.format(metricValues))
 
-
+print('Execution times: {0}'.format(executionTimes))
