c2819fc68a8ac13eb6bb5aa76ce90c3d3a7a7681
Martin Belzunce <martin.a.belzunce@gmail.com>
2020-09-15 11:06:25 +0100

    Bug in postprocessing labels and now added a removal of regions in 2d.

--- LabelPropagation/PostprocessingLabels.py
+++ LabelPropagation/PostprocessingLabels.py
@@ -14,7 +14,7 @@ import SitkImageManipulation as sitkIm
 
 # This function leaves only the largest region of the labels, first erodes the labels, then gets the largest unconnected
 # region and the dilate it.
-def FilterUnconnectedRegion(segmentedImage, numLabels):
+def FilterUnconnectedRegions(segmentedImage, numLabels):
     # Output image:
     outSegmentedImage = sitk.Image(segmentedImage.GetSize(), sitk.sitkUInt8)
     outSegmentedImage.CopyInformation(segmentedImage)
@@ -28,6 +28,8 @@ def FilterUnconnectedRegion(segmentedImage, numLabels):
         maskThisLabel = segmentedImage == (i+1)
         # Erode the mask:
         maskThisLabel = sitk.BinaryErode(maskThisLabel, radiusErodeDilate)
+        # Now resegment to get labels for each segmented object:
+        maskThisLabel = sitk.ConnectedComponent(maskThisLabel)
         # Relabel by object size:
         maskThisLabel = relabelFilter.Execute(maskThisLabel)
         # get the largest object:
@@ -37,5 +39,44 @@ def FilterUnconnectedRegion(segmentedImage, numLabels):
         # Assign to the output:
         maskFilter.SetOutsideValue(i+1)
         outSegmentedImage = maskFilter.Execute(outSegmentedImage, maskThisLabel)
+
+    return outSegmentedImage
+
+
+# This function leaves only the largest region for each slice of the labels.
+# For each labels, first erodes the labels, then gets the largest unconnected region and finally dilate it.
+def FilterUnconnectedRegionsPerSlices(segmentedImage, numLabels):
+    # Output image:
+    outSegmentedImage = sitk.Image(segmentedImage.GetSize(), sitk.sitkUInt8)
+    outSegmentedImage.CopyInformation(segmentedImage)
+    # Relabel filter:
+    relabelFilter = sitk.RelabelComponentImageFilter() # I use the relabel filter to get largest region for each label.
+    relabelFilter.SortByObjectSizeOn()
+    maskFilter = sitk.MaskImageFilter()
+    maskFilter.SetMaskingValue(1)
+    radiusErodeDilate=(1,1,0)
+    # Go through each slice:
+    for j in range(0, segmentedImage.GetSize()[2]):
+        segmentedSlice = segmentedImage[:,:,j]
+        outSlice = outSegmentedImage[:,:,j]
+        # Go through each label:
+        for i in range(0, numLabels):
+            maskThisLabel = segmentedSlice == (i+1)
+            # Erode the mask:
+            maskThisLabel = sitk.BinaryErode(maskThisLabel, radiusErodeDilate)
+            # Now resegment to get labels for each segmented object:
+            maskThisLabel = sitk.ConnectedComponent(maskThisLabel)
+            # Relabel by object size:
+            maskThisLabel = relabelFilter.Execute(maskThisLabel)
+            # get the largest object:
+            maskThisLabel = sitk.And(maskThisLabel>0, maskThisLabel<3)  # Leave the two largest objects (simple solution at the moment)
+            # dilate the mask:
+            maskThisLabel = sitk.BinaryDilate(maskThisLabel, radiusErodeDilate)
+            # Assign to the output:
+            maskFilter.SetOutsideValue(i+1)
+            outSlice = maskFilter.Execute(outSlice, maskThisLabel)
+            # Now paste the slice in the output:
+            outSlice = sitk.JoinSeries(outSlice) # Needs tobe a 3D image
+            outSegmentedImage = sitk.Paste(outSegmentedImage, outSlice, outSlice.GetSize(), destinationIndex=[0, 0, j])
 
     return outSegmentedImage
