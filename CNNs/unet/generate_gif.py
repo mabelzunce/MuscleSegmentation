29aa88f2643071f6287302e3ab7747812bb06508
German <gbalerdi@estudiantes.unsam.edu.ar>
2022-09-29 16:49:05 -0300

    generate gif, falta solapamiento

--- CNNs/unet/generate_gif.py
+++ CNNs/unet/generate_gif.py
@@ -27,7 +27,6 @@ extensionImages = 'mhd'
 tagLabels = '_labels'
 flag = 0
 
-fig,ax = plt.subplots()
 for images in trainingSet:
     name, extension = os.path.splitext(images)
     # Check if filename is the in phase header and the labels exists:
@@ -36,30 +35,20 @@ for images in trainingSet:
     if extension.endswith(extensionImages) and os.path.exists(filenameLabels):
         readImg = sitk.ReadImage(filenameImages)
         dataIm = sitk.GetArrayFromImage(readImg)
-        image = im.fromarray(dataIm)
-        #ts_imArray.append(image)
+        dataIm = dataIm - np.min(dataIm)
+        dataIm = 255 * (dataIm/(np.max(dataIm) - np.min(dataIm)))
+        img = dataIm.astype(np.uint8)
+        ts_imArray.append(img)
         readLb = sitk.ReadImage(filenameLabels)
         dataLb = sitk.GetArrayFromImage(readLb)
-        label = im.fromarray(dataLb)
-        ts_lbArray.append(label)
-        readImg = (readImg-np.min(dataIm))*1/(np.max(dataIm)-np.min(dataIm))
-        overlayImage = sitk.LabelOverlay(image=readImg,
-                                              labelImage=readLb,
-                                              opacity=0.5, backgroundValue=0)
-        ndaOverlayImage = sitk.GetArrayFromImage(overlayImage)
-        ax = plt.imshow(ndaOverlayImage)
-                    #, cmap='gray', vmin=0, vmax=0.5 * np.max(dataIm))
-        #plt.imshow(dataLb, cmap='hot', alpha=0.3)
-        #plt.axis('off')
-        plt.show(block=False)
-        plt.pause(0.1)
-        fusedIm = im.fromarray((256*ax.get_array()).astype(np.uint8), "RGB")
+        dataLb = dataLb - np.min(dataLb)
+        dataLb = 255 * (dataLb / (np.max(dataLb) - np.min(dataLb)))
+        lbl = dataLb.astype(np.uint8)
+        ts_lbArray.append(lbl)
 
-        ts_imArray.append(fusedIm)
-ts_imArray[0].save('trainingSet.gif', format="GIF", append_images=ts_imArray,
-       save_all=True, duration=300, loop=0)
-#imageio.mimwrite('trainingSet.gif', ts_imArray, 'GIF', duration=0.3)
-imageio.mimwrite('trainingSetLabels.gif', ts_lbArray, 'GIF', duration=0.3)
+
+imageio.mimwrite(trainingSetPath + 'trainingSet.gif', ls_imArray, 'GIF', duration=0.3)
+imageio.mimwrite(trainingSetPath + 'trainingSetLabels.gif', ls_lbArray, 'GIF', duration=0.3)
 
 for images in linearSet:
     name, extension = os.path.splitext(images)
@@ -69,18 +58,20 @@ for images in linearSet:
     if extension.endswith(extensionImages) and os.path.exists(filenameLabels):
         readImg = sitk.ReadImage(filenameImages)
         dataIm = sitk.GetArrayFromImage(readImg)
-        image = im.fromarray(dataIm)
-        ls_imArray.append(image)
+        dataIm = dataIm - np.min(dataIm)
+        dataIm = 255 * (dataIm/(np.max(dataIm) - np.min(dataIm)))
+        img = dataIm.astype(np.uint8)
+        ls_imArray.append(img)
         readLb = sitk.ReadImage(filenameLabels)
         dataLb = sitk.GetArrayFromImage(readLb)
-        label = im.fromarray(dataLb)
-        ls_lbArray.append(label)
+        dataLb = dataLb - np.min(dataLb)
+        dataLb = 255 * (dataLb / (np.max(dataLb) - np.min(dataLb)))
+        lbl = dataLb.astype(np.uint8)
+        ls_lbArray.append(lbl)
 
 
-imageio.mimwrite('linearSet.gif', ls_imArray, 'GIF', duration=0.2)
-imageio.mimwrite('linearSetLabels.gif', ls_lbArray, 'GIF', duration=0.2)
-
-
+imageio.mimwrite(linearPath + 'linearSet.gif', ls_imArray, 'GIF', duration=0.3)
+imageio.mimwrite(linearPath + 'linearSetLabels.gif', ls_lbArray, 'GIF', duration=0.3)
 
 
 
@@ -92,8 +83,8 @@ for images in nonlinearSet:
         flag = 1
 
     if auxName != name.split('_')[0]:
-        imageio.mimwrite(auxName + '.gif', ns_imArray, 'GIF', duration=0.2)
-        imageio.mimwrite(auxName + '_labels.gif', ns_lbArray, 'GIF', duration=0.2)
+        imageio.mimwrite(nonlinearPath + auxName + '.gif', ns_imArray, 'GIF', duration=0.3)
+        imageio.mimwrite(nonlinearPath + auxName + '_labels.gif', ns_lbArray, 'GIF', duration=0.3)
         ns_imArray = []
         ns_lbArray = []
 
@@ -104,12 +95,16 @@ for images in nonlinearSet:
     if extension.endswith(extensionImages) and os.path.exists(filenameLabels):
         readImg = sitk.ReadImage(filenameImages)
         dataIm = sitk.GetArrayFromImage(readImg)
-        image = im.fromarray(dataIm)
-        ns_imArray.append(image)
+        dataIm = dataIm - np.min(dataIm)
+        dataIm = 255 * (dataIm / (np.max(dataIm) - np.min(dataIm)))
+        img = dataIm.astype(np.uint8)
+        ns_imArray.append(img)
         readLb = sitk.ReadImage(filenameLabels)
         dataLb = sitk.GetArrayFromImage(readLb)
-        label = im.fromarray(dataLb)
-        ns_lbArray.append(label)
+        dataLb = dataLb - np.min(dataLb)
+        dataLb = 255 * (dataLb / (np.max(dataLb) - np.min(dataLb)))
+        lbl = dataLb.astype(np.uint8)
+        ns_lbArray.append(lbl)
 
-imageio.mimwrite(auxName + '.gif', ns_imArray, 'GIF', duration=0.2)
-imageio.mimwrite(auxName + '_labels.gif', ns_lbArray, 'GIF', duration=0.2)
+imageio.mimwrite(nonlinearPath + auxName + '.gif', ns_imArray, 'GIF', duration=0.3)
+imageio.mimwrite(nonlinearPath + auxName + '_labels.gif', ns_lbArray, 'GIF', duration=0.3)
