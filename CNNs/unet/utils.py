228300edb971f54c3a6ee9bf922cb4972fbaa5c4
German <gbalerdi@estudiantes.unsam.edu.ar>
2022-10-14 12:06:06 -0300

    funcion para guardar csv

--- CNNs/unet/utils.py
+++ CNNs/unet/utils.py
@@ -2,6 +2,7 @@ import SimpleITK
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 def imshow_from_torch(img, imin=0, imax=1, ialpha = 1, icmap='Greys'):
     #img = img / 2 + 0.5     # unnormalize
@@ -18,3 +19,17 @@ def swap_labels(img, label1=0, label2=1):
     img = (img * mask1not) + mask1 * label2
     img = (img * mask2not) + mask2 * label1
     return img
+
+def loss_csv (loss_vector, outpath):
+    data = []
+    for i in range(len(loss_vector)):
+        data.append([i, loss_vector[i]])
+    if "Epoch" in outpath:
+        header = ['Epoch', 'Loss']
+    else:
+        header = ['Iteration', 'Loss']
+    with open(outpath, 'w', newline='', encoding='UTF8') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(data)
+        file.close()
