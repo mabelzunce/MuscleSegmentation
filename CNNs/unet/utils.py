a5e1627fcf386b5ffa9a6acdfb5aad73cdd27129
German <gbalerdi@estudiantes.unsam.edu.ar>
2022-10-27 14:50:49 -0300

    dice Score added

--- CNNs/unet/utils.py
+++ CNNs/unet/utils.py
@@ -1,16 +1,18 @@
-import SimpleITK
+import SimpleITK as sitk
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+
 
 def imshow_from_torch(img, imin=0, imax=1, ialpha = 1, icmap='gray'):
     #img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)),vmin = imin, vmax = imax, cmap=icmap, alpha = ialpha)
 
+
 def swap_labels(img, label1=0, label2=1):
-    #img = img / 2 + 0.5     # unnormalize
+    #img = img / 2 + 0.5     #unnormalize
     mask1 = img == label1
     mask1not = img != label1
     mask2 = img == label2
@@ -18,7 +20,9 @@ def swap_labels(img, label1=0, label2=1):
     img = (img * mask1not) + mask1 * label2
     img = (img * mask2not) + mask2 * label1
     return img
-def loss_csv (loss_vector, outpath):
+
+
+def loss_csv(loss_vector, outpath):
     data = []
     for i in range(len(loss_vector)):
         data.append([i, loss_vector[i]])
@@ -31,3 +35,11 @@ def loss_csv (loss_vector, outpath):
         writer.writerow(header)
         writer.writerows(data)
         file.close()
+
+
+def dice(reference, segmented):
+    overlap_measures_filter = sitk.LabelOverlapMeasuresImageFilter()
+    overlap_measures_filter.SetGlobalDefaultCoordinateTolerance(1e-2)
+    overlap_measures_filter.Execute(reference, segmented)
+    score = overlap_measures_filter.GetDiceCoefficient()
+    return score
