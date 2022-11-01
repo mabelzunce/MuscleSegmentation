afc2d73e31f7e62f977a2ca177822ff3ce66fbfc
German <gbalerdi@estudiantes.unsam.edu.ar>
2022-11-01 16:00:33 -0300

    necesita revision

--- CNNs/unet/train_unet_2d.py
+++ CNNs/unet/train_unet_2d.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from utils import loss_csv
 from utils import imshow_from_torch
 from utils import dice
+from utils import dice2d
 from matplotlib import cm
 import torch
 import torchvision
@@ -58,8 +59,8 @@ tagLabels = '_labels'
 # imageSize_voxels = (256,256)
 
 # Training/dev sets ratio, not using test set at the moment:
-trainingSetRelSize = 0.8
-devSetRelSize = 0.2
+trainingSetRelSize = 0.6
+devSetRelSize = 1-trainingSetRelSize
 
 ######################### CHECK DEVICE ######################
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -75,17 +76,13 @@ atlasLabelsFilenames = [] # Filenames of the label images
 #imagesDataSet = np.empty()
 #labelsDataSet = np.empty()
 numImagesPerSubject = 0
+numRot = 0
+datasize = 0
 numSubjects = 1
 subject = files[0].split('_')[0]
-aux = subject
-
+subjects = []
+subjects.append(subject)
 for filename in files:
-    subjectName = filename.split('_')[0]
-    if subject == subjectName:
-        numImagesPerSubject += 1
-    if subjectName != aux:
-        numSubjects += 1
-        aux = subjectName
     name, extension = os.path.splitext(filename)
     # Substract the tagInPhase:
     atlasName = name
@@ -100,40 +97,85 @@ for filename in files:
         # Labels image:
         atlasLabelsFilenames.append(filenameLabels)
 
-numImagesPerSubject = int(np.trunc(numImagesPerSubject/4))
-# Initialize numpy array and read data:
-rints=[]
-numImages = 420 #numero divisible por  numero de sujetos e imagenes por sujeto
-imgPerSubject = int(np.trunc(numImages/numSubjects))
-print(imgPerSubject)
-rng = np.random.default_rng()
-for j in range(numSubjects):
-    rints.extend(rng.choice(numImagesPerSubject, size=imgPerSubject, replace=False))
+        # Data Set info
+        subjectName = filename.split('_')[0]
+        subjectAdd = filename.split('_')[1]
+        if subject == subjectName:
+            numImagesPerSubject += 1
+            if not (subjectName.startswith(subjectAdd[0])):
+                numRot += 1
+        if not (subjects.__contains__(subjectName)):
+            numSubjects += 1
+            subjects.append(subjectName)
+        datasize += 1
 
+tNum = int(np.round(numSubjects * trainingSetRelSize))
+vNum = int(np.round(numSubjects * devSetRelSize))
+trainingSubjects = subjects[:tNum]
+validSubjects = subjects[tNum:]
+
+trainingImagesNumber = (tNum + numRot) * tNum
+validImagesNumber = (vNum + numRot) * vNum
 k = 0
-for i in range(0, numImages):
-    # Read images and add them in a numpy array:
-    if ((i+1) % imgPerSubject) == 0 and (k+1) < numSubjects:
-        k += 1
-    atlasImage = sitk.ReadImage(atlasImageFilenames[k * numImagesPerSubject + rints[i]])
-    atlasLabels = sitk.ReadImage(atlasLabelsFilenames[k * numImagesPerSubject + rints[i]])
+j = 0
+for i in range(0, datasize):
+    name1, name2 = atlasNames[i].split('_')[:2]
+    condition1 = (trainingSubjects.__contains__(name1)) and (not (validSubjects.__contains__(name2)))
+    condition2 = (validSubjects.__contains__(name1)) and (not (trainingSubjects.__contains__(name2)))
+
+    atlasImage = sitk.ReadImage(atlasImageFilenames[i])
+    atlasLabels = sitk.ReadImage(atlasLabelsFilenames[i])
+
     if i == 0:
-        imagesDataSet = np.zeros([numImages,atlasImage.GetSize()[1],atlasImage.GetSize()[0]])
-        labelsDataSet = np.zeros([numImages,atlasImage.GetSize()[1],atlasImage.GetSize()[0]])
+        imagesTrainingSet = np.zeros([trainingImagesNumber, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        labelsTrainingSet = np.zeros([trainingImagesNumber, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        imagesValidSet = np.zeros([validImagesNumber, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        labelsValidSet = np.zeros([validImagesNumber, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
         # Size of each 2d image:
-        dataSetImageSize_voxels = imagesDataSet.shape[1:3]              #obtiene el getsize[1 y 0]
-    imagesDataSet[i, :, :] = np.reshape(sitk.GetArrayFromImage(atlasImage), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
-    labelsDataSet[i, :, :] = np.reshape(sitk.GetArrayFromImage(atlasLabels), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        dataSetImageSize_voxels = imagesTrainingSet.shape[1:3]  # obtiene el getsize[1 y 0]
+
+    if condition1:
+        imagesTrainingSet[k, :, :] = np.reshape(sitk.GetArrayFromImage(atlasImage), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        labelsTrainingSet[k, :, :] = np.reshape(sitk.GetArrayFromImage(atlasLabels), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        k += 1
+
+    if condition2:
+        imagesValidSet[j, :, :] = np.reshape(sitk.GetArrayFromImage(atlasImage), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        labelsValidSet[j, :, :] = np.reshape(sitk.GetArrayFromImage(atlasLabels), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        j += 1
+# Initialize numpy array and read data:
+#rints = []
+#numImages = 420 #numero divisible por  numero de sujetos e imagenes por sujeto
+#imgPerSubject = int(np.trunc(numImages/numSubjects))
+#print(imgPerSubject)
+#rng = np.random.default_rng()
+#for j in range(numSubjects):
+#    rints.extend(rng.choice(numImagesPerSubject, size=imgPerSubject, replace=False))
+
+#k = 0
+#for i in range(0, numImages):
+    # Read images and add them in a numpy array:
+#    if ((i+1) % imgPerSubject) == 0 and (k+1) < numSubjects:
+#        k += 1
+#    atlasImage = sitk.ReadImage(atlasImageFilenames[k * numImagesPerSubject + rints[i]])
+#    atlasLabels = sitk.ReadImage(atlasLabelsFilenames[k * numImagesPerSubject + rints[i]])
+#    if i == 0:
+#        imagesDataSet = np.zeros([numImages,atlasImage.GetSize()[1],atlasImage.GetSize()[0]])
+#        labelsDataSet = np.zeros([numImages,atlasImage.GetSize()[1],atlasImage.GetSize()[0]])
+        # Size of each 2d image:
+#        dataSetImageSize_voxels = imagesDataSet.shape[1:3]              #obtiene el getsize[1 y 0]
+#    imagesDataSet[i, :, :] = np.reshape(sitk.GetArrayFromImage(atlasImage), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+#    labelsDataSet[i, :, :] = np.reshape(sitk.GetArrayFromImage(atlasLabels), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
 
 print("Number of atlases images: {0}".format(len(atlasNames)))
 print("List of atlases: {0}\n".format(atlasNames))
 
 # Iterate and plot random images:
-numImagesToShow = numImages # Show all images
-cols = 6
-rows = int(np.ceil(numImagesToShow/cols))
-indicesImages = np.random.choice(numImages, numImagesToShow, replace=False)
-plt.figure(figsize=(15, 10))
+#numImagesToShow = numImages # Show all images
+#cols = 6
+#rows = int(np.ceil(numImagesToShow/cols))
+#indicesImages = np.random.choice(numImages, numImagesToShow, replace=False)
+#plt.figure(figsize=(15, 10))
 #for i in range(numImagesToShow):
     #plt.subplot(rows, cols, i + 1)
     #overlay = sitk.LabelOverlay(image=imagesDataSet[i,:,:],
@@ -151,28 +193,46 @@ plt.figure(figsize=(15, 10))
 
 # Add the channel dimension for compatibility:
 
-imagesDataSet = np.expand_dims(imagesDataSet, axis=1)
-labelsDataSet = np.expand_dims(labelsDataSet, axis=1)
+#imagesDataSet = np.expand_dims(imagesDataSet, axis=1)
+#labelsDataSet = np.expand_dims(labelsDataSet, axis=1)
+
+imagesTrainingSet = np.expand_dims(imagesTrainingSet, axis=1)
+labelsTrainingSet = np.expand_dims(labelsTrainingSet, axis=1)
+
+imagesValidSet = np.expand_dims(imagesValidSet, axis=1)
+labelsValidSet = np.expand_dims(labelsValidSet, axis=1)
 # Cast to float (the model expects a float):
-imagesDataSet = imagesDataSet.astype(np.float32)
-labelsDataSet = labelsDataSet.astype(np.float32)
-labelsDataSet[labelsDataSet != 1] = 0
-labelsDataSet[labelsDataSet == 1] = 1
+#imagesDataSet = imagesDataSet.astype(np.float32)
+#labelsDataSet = labelsDataSet.astype(np.float32)
+#labelsDataSet[labelsDataSet != 1] = 0
+#labelsDataSet[labelsDataSet == 1] = 1
+
+imagesTrainingSet = imagesTrainingSet.astype(np.float32)
+labelsTrainingSet = labelsTrainingSet.astype(np.float32)
+labelsTrainingSet[labelsTrainingSet != 1] = 0
+labelsTrainingSet[labelsTrainingSet == 1] = 1
+
+imagesValidSet = imagesValidSet.astype(np.float32)
+labelsValidSet = labelsValidSet.astype(np.float32)
+labelsValidSet[labelsValidSet != 1] = 0
+labelsValidSet[labelsValidSet == 1] = 1
+
 ######################## TRAINING, VALIDATION AND TEST DATA SETS ###########################
 # Get the number of images for the training and test data sets:
-sizeFullDataSet = int(imagesDataSet.shape[0])
-sizeTrainingSet = int(np.round(sizeFullDataSet*trainingSetRelSize))
-sizeDevSet = sizeFullDataSet-sizeTrainingSet
+#sizeFullDataSet = int(imagesDataSet.shape[0])
+#sizeTrainingSet = int(np.round(sizeFullDataSet*trainingSetRelSize))
+#sizeDevSet = sizeFullDataSet-sizeTrainingSet
 # Get random indices for the training set:
 rng = np.random.default_rng()
 #indicesTrainingSet = rng.choice(int(sizeFullDataSet), int(sizeTrainingSet), replace=False)
 #indicesDevSet = np.delete(range(sizeFullDataSet), indicesTrainingSet)
-indicesTrainingSet = range(0,int(sizeTrainingSet))
-indicesDevSet = range(int(sizeTrainingSet), sizeFullDataSet)
+#indicesTrainingSet = range(0, int(sizeTrainingSet))
+#indicesDevSet = range(int(sizeTrainingSet), sizeFullDataSet)
 # Create dictionaries with training sets:
-trainingSet = dict([('input', imagesDataSet[indicesTrainingSet, :, :, :]), ('output', labelsDataSet[indicesTrainingSet, :, :, :])])
-devSet = dict([('input', imagesDataSet[indicesDevSet, :, :, :]), ('output', labelsDataSet[indicesDevSet,:,:,:])])
-
+#trainingSet = dict([('input', imagesDataSet[indicesTrainingSet, :, :, :]), ('output', labelsDataSet[indicesTrainingSet, :, :, :])])
+#devSet = dict([('input', imagesDataSet[indicesDevSet, :, :, :]), ('output', labelsDataSet[indicesDevSet,:,:,:])])
+trainingSet = dict([('input', imagesTrainingSet[:, :, :, :]), ('output', labelsTrainingSet[:, :, :, :])])
+devSet = dict([('input', imagesValidSet[:, :, :, :]), ('output', labelsValidSet[:,:,:,:])])
 print('Data set size. Training set: {0}. Dev set: {1}.'.format(trainingSet['input'].shape[0], devSet['input'].shape[0]))
 
 ####################### CREATE A U-NET MODEL #############################################
@@ -270,21 +330,23 @@ for epoch in range(50):  # loop over the dataset multiple times
         iterationNumbers.append(iter)
         #Print epoch iteration and loss value:
         print('[%d, %5d] loss: %.3f' % (epoch, i, loss.item()))
-        #running_loss = 0.0
         # Update iteration number:
         iter = iter + 1
         loss_csv(lossValuesTrainingSet, outputPath + 'TestDataIter.csv')
+        reference = gt.cpu().numpy()
+        reference = reference.astype('int64') * 255
+        labels = torch.sigmoid(outputs.cpu().to(torch.float32))
+        labels = (labels > 0.5) * 255
+        labels = labels.numpy()
         for k in range(batchSize):
-            reference = trainingSet['output'][i*batchSize + k, 0, :, :]
-            reference = sitk.GetImageFromArray(reference.astype('int64'))
-            outputs = outputs.cpu()
-            labels = torch.sigmoid(outputs.to(torch.float32))
-            labels = (labels > 0.5) * 255
-            segmented = sitk.GetImageFromArray(labels.numpy()[k, 0, :, :])
-            diceTraining.append(dice(reference, segmented))
-            print('diceScore: %d' % diceTraining[k])
+            ref = reference[k, 0, :, :]
+            seg = labels[k, 0, :, :]
+            diceScore = dice2d(ref, seg)
+            diceTraining.append(diceScore)
     diceTrainingEpoch.append(np.mean(diceTraining))
+    print('Training Dice Score: %f ' % np.mean(diceTraining))
     lossValuesTrainingSetAllEpoch.append(np.mean(lossValuesTrainingSetEpoch))
+    loss_csv(diceTrainingEpoch, outputPath + 'TrainingDiceEpoch.csv')
     loss_csv(lossValuesTrainingSetAllEpoch, outputPath + 'TestDataEpoch.csv')
 
     unet.train(False)
@@ -296,7 +358,6 @@ for epoch in range(50):  # loop over the dataset multiple times
 
             outputs = unet(inputs)
             loss = criterion(outputs, gt)
-            #loss.backward()
 
             lossValuesDevSet.append(loss.item())
             lossValuesDevSetEpoch.append(loss.item())
@@ -304,8 +365,21 @@ for epoch in range(50):  # loop over the dataset multiple times
             iterationDevNumbers.append(deviter)
             deviter = deviter + 1
             loss_csv(lossValuesDevSet, outputPath + 'ValidDataIter.csv')
+        reference = gt.cpu().numpy()
+        reference = reference.astype('int64') * 255
+        labels = torch.sigmoid(outputs.cpu().to(torch.float32))
+        labels = (labels > 0.5) * 255
+        labels = labels.numpy()
+        for k in range(devBatchSize):
+            ref = reference[k, 0, :, :]
+            seg = labels[k, 0, :, :]
+            diceScore = dice2d(ref, seg)
+            diceValid.append(diceScore)
+    diceValidEpoch.append(np.mean(diceValid))
+    print('Valid Dice Score:  %f ' % np.mean(diceValid))
     avg_vloss = np.mean(lossValuesDevSetEpoch)
     lossValuesDevSetAllEpoch.append(avg_vloss)
+    loss_csv(diceValidEpoch, outputPath + 'ValidDiceEpoch.csv')
     loss_csv(lossValuesDevSetAllEpoch, outputPath + 'ValidDataEpoch.csv')
 
     if (epoch % plotStep_epochs) == (plotStep_epochs - 1):
@@ -331,14 +405,10 @@ for epoch in range(50):  # loop over the dataset multiple times
                           ialpha=0.5, icmap='hot')
         axs_epochs[1].set_title('Input - Output UNET Batch {0}, Epoch {1}'.format(i, epoch))
         plt.axes(axs_epochs[2])
-        #plt.imshow((inputs.cpu())[0, 0, :, :], cmap='gray', vmin=0, vmax=0.5 * np.max(imagesDataSet[i, :, :]))
-        #plt.imshow((outputsLabels.cpu().detach())[0,0,:,:], cmap='hot', alpha=0.5)
-        #plt.imshow(inputs.cpu(), cmap='hot', alpha=0.3)
         imshow_from_torch(torchvision.utils.make_grid(inputs.cpu(), normalize=True, value_range=(0,0.5 * torch.max(inputs.cpu())), nrow=numImagesPerRow), icmap='gray')
         cmap_vol = np.apply_along_axis(cm.hot, 0, outputsLabels.cpu().detach().numpy())  # converts prediction to cmap!
         cmap_vol = torch.from_numpy(np.squeeze(cmap_vol))
         imshow_from_torch(torchvision.utils.make_grid(cmap_vol, nrow=numImagesPerRow), ialpha=0.3, icmap='hot')
-        #imshow_from_torch(torchvision.utils.make_grid(outputsLabels.cpu().detach(), normalize=False, nrow=numImagesPerRow), ialpha=0.3, icmap='hot')
         axs_epochs[2].set_title('Input - Output Labels Batch {0}, Epoch {1}'.format(i, epoch))
         plt.axes(axs_epochs[3])
         imshow_from_torch(torchvision.utils.make_grid(gt.cpu(), normalize=True, nrow=numImagesPerRow))
@@ -346,10 +416,7 @@ for epoch in range(50):  # loop over the dataset multiple times
             torchvision.utils.make_grid(outputsLabels.cpu().detach(), normalize=False, nrow=numImagesPerRow),
             ialpha=0.5, icmap='hot')
         axs_epochs[3].set_title('Ground Truth - Output Labels')
-        #plt.draw()
-        #plt.pause(0.0001)
         plt.savefig(outputPath + 'model_training_epoch_{0}.png'.format(epoch))
-
 
     if avg_vloss < best_vloss:
         best_vloss = avg_vloss
