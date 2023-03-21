48dc0dd0e396c99827553140c531e23b35e7c7fa
German <gbalerdi@estudiantes.unsam.edu.ar>
2023-03-20 23:05:30 -0300

    16 filter setup

---
+++ CNNs/unet/Test_2dunet_multilabel.py
@@ -0,0 +1,403 @@
+import SimpleITK as sitk
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import math
+from datetime import datetime
+from utils import create_csv
+from utils import imshow_from_torch
+from utils import dice2d
+from utils import specificity
+from utils import sensitivity
+from utils import maxProb
+from utils import writeMhd
+from utils import multilabel
+from utils import boxplot
+from utils import DiceLoss
+from utils import pn_weights
+from utils import rel_weights
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.utils import draw_segmentation_masks
+import torch.optim as optim
+
+from torch import nn
+from torch.utils.data import DataLoader
+
+
+#from sklearn.model_selection import train_test_split
+
+
+from unet_2d import Unet
+#from utils import imshow
+#from utils import MSE
+import torch
+import torch.nn as nn
+import torchvision
+
+import torch.nn.functional as F
+from torchvision.utils import make_grid
+
+class Augment(enumerate):
+    NA = 0  # No Augment
+    NL = 1  # Non linear
+    L = 2  # Linear
+    A = 3   #Augmented
+
+
+saveMhd = True        # Saves a mhd file for the output
+saveDataSetMhd = False  # Saves a Mhd file of the images and labels from dataset
+Background = False        # Background is considered as label
+Boxplot = True           # Boxplot created in every best fit
+AugmentedTrainingSet = Augment.NA
+# Para correr la prueba corroborar que cantidad de filtros  establecidos  en "unet_2d" es igual a los del modelo
+
+############################ DATA PATHS ##############################################
+trainingSetPath = '..\\..\\Data\\LumbarSpine2D\\TrainingSet\\'
+outputPath = '..\\..\\Data\\LumbarSpine2D\\model\\'
+modelLocation = '..\\..\\Data\\LumbarSpine2D\\PretrainedModel\\'
+
+
+modelName = os.listdir(modelLocation)[0]
+unetFilename = modelLocation + modelName
+
+if not os.path.exists(outputPath):
+    os.makedirs(outputPath)
+
+# Image format extension:
+extensionShortcuts = 'lnk'
+strForShortcut = '-> '
+extensionImages = 'mhd'
+tagInPhase = '_I'
+tagLabels = '_labels'
+############################ PARAMETERS ################################################
+# Size of the image we want to use in the cnn.
+# We will get it from the training set.
+# imageSize_voxels = (256,256)
+
+# Training/dev sets ratio, not using test set at the moment:
+trainingSetRelSize = 0.7
+devSetRelSize = 1-trainingSetRelSize
+
+######################### CHECK DEVICE ######################
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+###################### READ DATA AND PRE PROCESS IT FOR TRAINING DATA SETS #####################################################
+# Look for the folders or shortcuts:
+files = os.listdir(trainingSetPath)
+atlasNames = [] # Names of the atlases
+atlasImageFilenames = [] # Filenames of the intensity images
+atlasLabelsFilenames = [] # Filenames of the label images
+
+numImagesPerSubject = 0
+numRot = 0
+datasize = 0
+numSubjects = 1
+subject = files[0].split('_')[0]
+subjects = []
+subjects.append(subject)
+for filename in files:
+    name, extension = os.path.splitext(filename)
+    # Substract the tagInPhase:
+    atlasName = name
+    # Check if filename is the in phase header and the labels exists:
+    filenameImages = trainingSetPath + filename
+    filenameLabels = trainingSetPath + atlasName + tagLabels + '.' + extensionImages
+    if extension.endswith(extensionImages) and os.path.exists(filenameLabels):
+        # Atlas name:
+        atlasNames.append(atlasName)
+        # Intensity image:
+        atlasImageFilenames.append(filenameImages)
+        # Labels image:
+        atlasLabelsFilenames.append(filenameLabels)
+
+        # Data Set info
+        subjectName = filename.split('_')[0]
+        subjectAdd = filename.split('_')[1]
+        if subject == subjectName:
+            numImagesPerSubject += 1
+            if not (subjectName.startswith(subjectAdd[0])):
+                numRot += 1
+        if not (subjects.__contains__(subjectName)):
+            numSubjects += 1
+            subjects.append(subjectName)
+        datasize += 1
+
+tNum = int(np.floor(numSubjects * trainingSetRelSize))
+vNum = int(np.ceil(numSubjects * devSetRelSize))
+trainingSubjects = subjects[:tNum]
+validSubjects = subjects[tNum:]
+match AugmentedTrainingSet:
+    case 1:
+        tNum = (tNum * tNum)
+    case 2:
+        tNum = (numRot * tNum)
+    case 3:
+        tNum = (tNum + numRot) * tNum
+
+k = 0
+j = 0
+# Data set avoids mixing same subject images
+for i in range(0, datasize):
+    name1, name2 = atlasNames[i].split('_')[:2]
+    match AugmentedTrainingSet:
+        case 0:
+            condition1 = (trainingSubjects.__contains__(name1)) and (name2 == name1)
+        case 1:
+            condition1 = (trainingSubjects.__contains__(name1)) and (trainingSubjects.__contains__(name2))
+        case 2:
+            conditionAux = trainingSubjects.__contains__(name2) or validSubjects.__contains__(name2)
+            condition1 = ((trainingSubjects.__contains__(name1)) and not conditionAux)
+        case 3:
+            condition1 = (trainingSubjects.__contains__(name1)) and (not (validSubjects.__contains__(name2)))
+
+    condition2 = (validSubjects.__contains__(name1)) and (name2 == name1)
+
+    atlasImage = sitk.ReadImage(atlasImageFilenames[i])
+    atlasLabels = sitk.ReadImage(atlasLabelsFilenames[i])
+
+    trainingSetShape = [tNum, atlasImage.GetSize()[1], atlasImage.GetSize()[0]]
+    validSetShape = [vNum, atlasImage.GetSize()[1], atlasImage.GetSize()[0]]
+
+    if i == 0:
+        imagesTrainingSet = np.zeros(trainingSetShape)
+        labelsTrainingSet = np.zeros(trainingSetShape)
+        imagesValidSet = np.zeros(validSetShape)
+        labelsValidSet = np.zeros(validSetShape)
+        # Size of each 2d image:
+        dataSetImageSize_voxels = imagesTrainingSet.shape[1:3]  # obtiene el getsize[1 y 0]
+
+    if condition1:
+        imagesTrainingSet[k, :, :] = np.reshape(sitk.GetArrayFromImage(atlasImage), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        labelsTrainingSet[k, :, :] = np.reshape(sitk.GetArrayFromImage(atlasLabels), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        k += 1
+
+    if condition2:
+        imagesValidSet[j, :, :] = np.reshape(sitk.GetArrayFromImage(atlasImage), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        labelsValidSet[j, :, :] = np.reshape(sitk.GetArrayFromImage(atlasLabels), [1, atlasImage.GetSize()[1], atlasImage.GetSize()[0]])
+        j += 1
+if saveDataSetMhd:
+    writeMhd(imagesTrainingSet.astype(np.float32), outputPath + 'images_training_set.mhd')
+    writeMhd(labelsTrainingSet.astype(np.uint8), outputPath + 'labels_training_set.mhd')
+    writeMhd(imagesValidSet.astype(np.float32), outputPath + 'images_valid_set.mhd')
+    writeMhd(labelsValidSet.astype(np.uint8), outputPath + 'labels_valid_set.mhd')
+# Initialize numpy array and read data:
+print("Number of atlases images: {0}".format(len(atlasNames)))
+print("List of atlases: {0}\n".format(atlasNames))
+
+
+# Add the channel dimension for compatibility:
+imagesTrainingSet = np.expand_dims(imagesTrainingSet, axis=1)
+labelsTrainingSet = np.expand_dims(labelsTrainingSet, axis=1)
+
+imagesValidSet = np.expand_dims(imagesValidSet, axis=1)
+labelsValidSet = np.expand_dims(labelsValidSet, axis=1)
+
+# Cast to float (the model expects a float):
+imagesTrainingSet = imagesTrainingSet.astype(np.float32)
+labelsTrainingSet = labelsTrainingSet.astype(np.float32)
+
+
+imagesValidSet = imagesValidSet.astype(np.float32)
+labelsValidSet = labelsValidSet.astype(np.float32)
+
+
+######################## TRAINING, VALIDATION AND TEST DATA SETS ###########################
+trainingSet = dict([('input', imagesTrainingSet[:, :, :, :]), ('output', labelsTrainingSet[:, :, :, :])])
+devSet = dict([('input', imagesValidSet[:, :, :, :]), ('output', labelsValidSet[:,:,:,:])])
+print('Data set size. Training set: {0}. Dev set: {1}.'.format(trainingSet['input'].shape[0], devSet['input'].shape[0]))
+labelNames = ('Background', 'Left Multifidus', 'Right Multifidus', 'Left Quadratus', 'Right Quadratus', 'Left Psoas', 'Right Psoas')
+####################### CREATE A U-NET MODEL #############################################
+# Create a UNET with one input and multiple output canal.
+multilabelNum = 6
+if Background:
+    multilabelNum += 1
+    xLabel = ['BG', 'LM', 'RM', 'LQ', 'RQ', 'LP', 'RP']
+    pos_weights = rel_weights(labelsTrainingSet, multilabelNum)
+    pos_weights = pos_weights.to(device)
+    criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weights)
+else:
+    xLabel = ['LM', 'RM', 'LQ', 'RQ', 'LP', 'RP']
+    criterion = nn.BCEWithLogitsLoss()
+
+unet = Unet(1, multilabelNum)
+unet.load_state_dict(torch.load(unetFilename, map_location=device))
+
+inp = torch.rand(1, 1, dataSetImageSize_voxels[0], dataSetImageSize_voxels[1])
+out = unet(inp)
+
+##
+print('Test Unet Input/Output sizes:\n Input size: {0}.\n Output shape: {1}'.format(inp.shape, out.shape))
+#tensorGroundTruth.shape
+##################################### U-NET TRAINING ############################################
+# Number of  batches:
+batchSize = 4
+devBatchSize = 4
+numBatches = np.ceil(trainingSet['input'].shape[0]/batchSize).astype(int)
+devNumBatches = np.ceil(devSet['input'].shape[0]/devBatchSize).astype(int)
+# Show results every printStep batches:
+plotStep_epochs = 1
+numImagesPerRow = batchSize
+if plotStep_epochs != math.inf:
+    figGraphs, axs_graphs = plt.subplots(1, 8, figsize=(15, 8))
+# Show dev set loss every showDevLossStep batches:
+#showDevLossStep = 1
+inputsDevSet = torch.from_numpy(devSet['input'])
+gtDevSet = torch.from_numpy(devSet['output'])
+# Train
+best_vloss = 1
+
+
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+iterationNumbers = []
+iterationDevNumbers = []
+epochNumbers = []
+
+lossValuesTrainingSet = []
+lossValuesDevSet = []
+
+lossValuesTrainingSetAllEpoch = []
+lossValuesDevSetAllEpoch = []
+
+
+torch.cuda.empty_cache()
+unet.to(device)
+if saveMhd:
+    outputTrainingSet = np.zeros(trainingSetShape)
+    outputValidSet = np.zeros(validSetShape)
+
+lossValuesTrainingSetEpoch = []
+lossValuesDevSetEpoch = []
+
+diceTraining = [[] for n in range(multilabelNum)]
+diceValid = [[] for n in range(multilabelNum)]
+
+diceTrainingEpoch = [[] for n in range(multilabelNum)]
+diceValidEpoch = [[] for n in range(multilabelNum)]
+#### TRAINING ####
+
+unet.train(False)
+for i in range(numBatches):
+    # get the inputs
+    with torch.no_grad():
+        inputs = torch.from_numpy(trainingSet['input'][i*batchSize:(i+1)*batchSize, :, :, :]).to(device)
+        gt = torch.from_numpy(trainingSet['output'][i*batchSize:(i+1)*batchSize, :, :, :]).to(device)
+        gt = F.one_hot(gt.to(torch.int64))
+        gt = torch.squeeze(torch.transpose(gt, 1, 4), 4)
+        gt = gt.float()
+        if not Background:
+            gt = gt[:, 1:, :, :]
+
+        outputs = unet(inputs)
+        loss = criterion(outputs, gt)
+
+        lossValuesTrainingSet.append(loss.item())
+        lossValuesTrainingSetEpoch.append(loss.item())
+
+    #Print epoch iteration and loss value:
+
+    create_csv(lossValuesTrainingSet, outputPath + 'TestLossIter.csv')
+    label = gt.cpu().numpy()
+    label = label.astype('int32')
+    segmentation = torch.sigmoid(outputs.cpu().to(torch.float32))
+    segmentation = maxProb(segmentation.detach().numpy(), multilabelNum)
+    segmentation = (segmentation > 0.5) * 1
+    if saveMhd:
+        outputTrainingSet[i*batchSize:(i+1)*batchSize] = multilabel(segmentation, multilabelNum, Background)
+
+    for k in range(label.shape[0]):
+        for j in range(multilabelNum):
+            lbl = label[k, j, :, :]
+            seg = segmentation[k, j, :, :]
+            diceScore = dice2d(lbl, seg)
+            diceTraining[j].append(diceScore)
+
+for j in range(multilabelNum):
+    diceTrainingEpoch[j].append(np.mean(diceTraining[j]))
+    print('Training Dice Score: %f ' % np.mean(diceTraining[j]))
+
+avg_tloss = np.mean(lossValuesTrainingSetEpoch)
+lossValuesTrainingSetAllEpoch.append(avg_tloss)
+print('avg_tloss: %f' % avg_tloss)
+
+for k in range(multilabelNum):
+    create_csv(diceTrainingEpoch[k], outputPath + 'TrainingDice_' + labelNames[k] + '.csv')
+create_csv(lossValuesTrainingSetAllEpoch, outputPath + 'TestLossEpoch.csv')
+
+
+
+#### VALIDATION ####
+unet.train(False)
+torch.cuda.empty_cache()
+for i in range(devNumBatches):
+    with torch.no_grad():
+        inputs = torch.from_numpy(devSet['input'][i * devBatchSize:(i + 1) * devBatchSize, :, :, :]).to(device)
+        gt = torch.from_numpy(devSet['output'][i * devBatchSize:(i + 1) * devBatchSize, :, :, :]).to(device)
+        gt = F.one_hot(gt.to(torch.int64))
+        gt = torch.squeeze(torch.transpose(gt, 1, 4), 4)
+        gt = gt.float()
+        if not Background:
+            gt = gt[:, 1:, :, :]
+
+        outputs = unet(inputs)
+        loss = criterion(outputs, gt)
+
+        lossValuesDevSet.append(loss.item())
+        lossValuesDevSetEpoch.append(loss.item())
+
+        create_csv(lossValuesDevSet, outputPath + 'ValidLossIter.csv')
+
+    label = gt.cpu().numpy()
+    label = label.astype('int32')
+    segmentation = torch.sigmoid(outputs.cpu().to(torch.float32))
+    segmentation = maxProb(segmentation.detach().numpy(), multilabelNum)
+    segmentation = (segmentation > 0.5) * 1
+    if saveMhd:
+        outputValidSet[i * devBatchSize:(i + 1) * devBatchSize] = multilabel(segmentation, multilabelNum, Background)
+
+    for k in range(label.shape[0]):
+        for j in range(multilabelNum):
+            lbl = label[k, j, :, :]
+            seg = segmentation[k, j, :, :]
+            diceScore = dice2d(lbl, seg)
+            specScore = specificity(lbl, seg)
+            sensScore = sensitivity(lbl, seg)
+            diceValid[j].append(diceScore)
+for j in range(multilabelNum):
+    diceValidEpoch[j].append(np.mean(diceValid[j]))
+    print('Valid Dice Score:  %f ' % np.mean(diceValid[j]))
+
+avg_vloss = np.mean(lossValuesDevSetEpoch)
+lossValuesDevSetAllEpoch.append(avg_vloss)
+print('avg_vloss: %f' % avg_vloss)
+
+for k in range(multilabelNum):
+    create_csv(diceValidEpoch[k], outputPath + 'ValidDice_' + labelNames[k] + '.csv')
+create_csv(lossValuesDevSetAllEpoch, outputPath + 'ValidLossEpoch.csv')
+
+
+#boxplot:
+if Boxplot:
+    boxplot(diceTraining[1:], xlabel=xLabel[1:],
+            outpath=(outputPath + 'trainingBoxplot.png'), yscale=[0, 1], title='Training Dice Scores')
+    boxplot(diceTraining[1:], xlabel=xLabel[1:],
+            outpath=(outputPath + 'trainingBoxplot_shortScale.png'), yscale=[0.7, 1.0], title='Training Dice Scores')
+    boxplot(diceValid[1:], xlabel=xLabel[1:],
+            outpath=(outputPath + 'validBoxplot.png'), yscale=[0, 1], title='Validation Dice Scores')
+    for k in range(multilabelNum):
+        boxplot(data=(diceTraining[k], diceValid[k]),
+                xlabel=['Training Set', 'Valid Set'], outpath=(outputPath + labelNames[k] + '_boxplot.png'),
+                yscale=[0.7, 1.0], title=labelNames[k]+' Dice Scores')
+if saveMhd:
+    writeMhd(outputTrainingSet.astype(np.uint8), outputPath + 'outputTrainingSet.mhd')
+    writeMhd(outputValidSet.astype(np.uint8), outputPath + 'outputValidSet.mhd')
+print('Test Finished')
+
+
+
+
+
+
---
+++ CNNs/unet/generate_training_set_3d.py
@@ -0,0 +1,218 @@
+#! python3
+import SimpleITK
+import SimpleITK as sitk
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import os
+from utils import swap_labels
+#import winshell
+
+############################### CONFIGURATION #####################################
+DEBUG = 0 # In debug mode, all the intermediate iamges are written.
+USE_COSINES_AND_ORIGIN = 1
+
+############################## REGISTRATION PARAMETER FILES ######################
+similarityMetricForReg = 'NMI'
+parameterFilesPath = '..\\..\\Data\\Elastix\\'
+paramFileRigid = 'Parameters_Rigid_' + similarityMetricForReg
+paramFileAffine = 'Parameters_Affine_' + similarityMetricForReg
+paramFileNonRigid = 'Parameters_BSpline_NCC_1000iters_2048samples'#Par0000bspline_500'
+############################## AUGMENTATION PARAMETERS ##########################
+rotationValues_deg = range(-10, 10+1, 5)
+############################### IMAGES AVAILABLE ###################################
+
+dataPath = '../../Data/LumbarSpine3D/Segmented/' # Base data path.
+outputPath = '../../Data/LumbarSpine3D/TrainingSet/' # Base data path.
+outputAugmentedLinearPath = '../../Data/LumbarSpine3D/TrainingSetAugmentedLinear/' # Base data path.
+outputAugmentedNonLinearPath = '../../Data/LumbarSpine3D/TrainingSetAugmentedNonLinear/' # Base data path.
+if not os.path.exists(outputPath):
+    os.makedirs(outputPath)
+if not os.path.exists(outputAugmentedLinearPath):
+    os.makedirs(outputAugmentedLinearPath)
+if not os.path.exists(outputAugmentedNonLinearPath):
+    os.makedirs(outputAugmentedNonLinearPath)
+# Get the atlases names and files:
+# Look for the folders or shortcuts:
+data = os.listdir(dataPath)
+# Image format extension:
+extensionShortcuts = 'lnk'
+strForShortcut = '-> '
+extensionImages = 'mhd'
+tagInPhase = '_I'
+tagAutLabels = '_aut'
+tagManLabels = '_labels'
+atlasNames = [] # Names of the atlases
+atlasImageFilenames = [] # Filenames of the intensity images
+atlasLabelsFilenames = [] # Filenames of the label images
+folderIndex = []
+
+#for folder in data:
+#    auxPath = dataPath + folder + '\\'
+#    files = os.listdir(auxPath)
+for filename in data:
+    name, extension = os.path.splitext(filename)
+    # Substract the tagInPhase:
+    atlasName = name[:-len(tagInPhase)]
+
+    # Check if filename is the in phase header and the labels exists:
+    filenameImages = dataPath + atlasName + tagInPhase + '.' + extensionImages
+    filenameManLabels = dataPath + atlasName + tagManLabels + '.' + extensionImages
+    if name.endswith(tagInPhase) and extension.endswith(extensionImages) and os.path.exists(filenameManLabels):
+        #\ and (atlasName not in atlasNamesImplantOrNotGood):
+        # Atlas name:
+        atlasNames.append(atlasName)
+        # Intensity image:
+        atlasImageFilenames.append(filenameImages)
+        # Manual Labels image:
+        atlasLabelsFilenames.append(filenameManLabels)
+
+print("Number of atlases images: {0}".format(len(atlasNames)))
+print("List of atlases: {0}\n".format(atlasNames))
+
+
+################################### REFERENCE IMAGE FOR THE REGISTRATION #######################
+indexReference = 0
+referenceSliceImage = sitk.ReadImage(atlasImageFilenames[indexReference])    #Es una Reference image no un Reference slice image
+print('Reference image: {0}. Voxel size: {1}'.format(atlasImageFilenames[indexReference], referenceSliceImage.GetSize()))
+
+################################### READ IMAGES, EXTRACT SLICES AND REGISTER IMAGES TO THE REFERENCE ########################################
+for i in range(0, len(atlasNames)):
+    print('Altas:{0}\n'.format(atlasImageFilenames[i]))
+    ############## 1) READ IMAGE WITH LABELS #############     #poner 3
+    # Read target image:
+    atlasSliceImage = sitk.ReadImage(atlasImageFilenames[i])
+    atlasSliceManLabel = sitk.ReadImage(atlasLabelsFilenames[i])
+
+    # Cast the image as float:
+    atlasSliceImage = sitk.Cast(atlasSliceImage, sitk.sitkFloat32)   #lo convierte en float 32
+    # Rigid registration to match voxel size and FOV.
+    ############## 1) RIGID REGISTRATION #############
+    # elastixImageFilter filter
+    elastixImageFilter = sitk.ElastixImageFilter()
+    # Parameter maps:
+    parameterMapVector = sitk.VectorOfParameterMap()
+    parameterMapVector.append(elastixImageFilter.ReadParameterFile(parameterFilesPath + paramFileRigid + '.txt'))
+    # Registration:
+    elastixImageFilter.SetFixedImage(referenceSliceImage)
+    elastixImageFilter.SetMovingImage(atlasSliceImage)
+    elastixImageFilter.SetParameterMap(parameterMapVector)
+    elastixImageFilter.Execute()
+    # Get result and apply transform to labels:
+    # Get the images:
+    atlasSliceImage = elastixImageFilter.GetResultImage()
+    # Apply transform:
+    transformixImageFilter = sitk.TransformixImageFilter()
+    transformixImageFilter.SetMovingImage(atlasSliceManLabel)
+    transformixImageFilter.SetTransformParameterMap(elastixImageFilter.GetTransformParameterMap())
+    transformixImageFilter.SetTransformParameter("FinalBSplineInterpolationOrder", "0")
+    transformixImageFilter.SetTransformParameter("ResultImagePixelType", "unsigned char")
+    transformixImageFilter.Execute()
+    transformixImageFilter.SetMovingImage(atlasSliceManLabel)
+    transformixImageFilter.Execute()
+    atlasSliceManLabel = sitk.Cast(transformixImageFilter.GetResultImage(), sitk.sitkUInt8)
+    # Crop The resulting image
+
+    # atlasSliceImage = atlasSliceImage[100:700, 160:480, :]
+    # atlasSliceManLabel = atlasSliceManLabel[100:700, 160:480, :]
+
+    # write the 3d images:
+    sitk.WriteImage(atlasSliceImage, outputPath + atlasNames[i] + '.' + extensionImages)
+    sitk.WriteImage(atlasSliceManLabel, outputPath + atlasNames[i] + tagManLabels + '.' + extensionImages)
+    # Show images:
+    if DEBUG:
+        slice = sitk.GetArrayFromImage(atlasSliceImage)
+        manLabels = sitk.GetArrayFromImage(atlasSliceManLabel)
+        plt.subplot(1,3,1)
+        plt.imshow(slice, cmap='gray')
+        plt.imshow(manLabels, cmap='hot', alpha=0.5)
+
+    ################################### AUGMENTATE WITH REFLECTION AND ROTATION ########################################
+    for reflectionX in [-1, 1]:
+        ############## Reflection ######################
+        imageArray = sitk.GetArrayFromImage(referenceSliceImage)
+        imageCenter_mm = np.array(referenceSliceImage.GetSpacing()) * np.array(referenceSliceImage.GetSize())/2; #0.5 * len(imageArray), 0.5 * len(imageArray[0])]
+        scale = SimpleITK.ScaleTransform(3, (reflectionX, 1, 1))   #chequear si quiero q refleje en x #A 2D or 3D anisotropic scale of coordinate space around a fixed center.
+        scale.SetCenter(imageCenter_mm)
+        #if reflectionX == -1:
+        #    filter = sitk.FlipImageFilter()
+        #    filter.SetFlipAxes((True, False))
+        #    atlasSliceImageTransformed = filter.Execute(atlasSliceImage)
+        #    atlasSliceLabelTransformed = filter.Execute(atlasSliceLabel)
+        #else:
+        #    atlasSliceImageTransformed = atlasSliceImage
+        #    atlasSliceLabelTransformed = atlasSliceLabel
+        for rotAngle_deg in rotationValues_deg: #rotationValues_deg (definidos antes)= range(-10, 10+1, 5)
+            rotation3D = sitk.Euler3DTransform()
+            #rotation2D.SetAngle(np.deg2rad(rotAngle_deg))
+            rotation3D.SetRotation(0, 0, np.deg2rad(rotAngle_deg))
+            rotation3D.SetCenter(imageCenter_mm)
+            # Composite transform: (junta las dos transofrmadas)
+            composite = sitk.Transform(scale)
+            composite.AddTransform(rotation3D)
+            #scale.SetScale((-1,1))
+            # Apply transform:
+            atlasSliceImageTransformed = sitk.Resample(atlasSliceImage, composite, sitk.sitkLinear, 0)
+            atlasSliceManLabelTransformed = sitk.Resample(atlasSliceManLabel, composite, sitk.sitkNearestNeighbor, 0,sitk.sitkUInt8)
+            # Change the labels side:
+            if reflectionX == -1:
+                for l in range(1, 6, 2):
+                    atlasSliceManLabelTransformed = swap_labels(atlasSliceManLabelTransformed, label1=l, label2=l + 1)
+            # write the 2d images:
+            sitk.WriteImage(atlasSliceImageTransformed, outputAugmentedLinearPath + atlasNames[i] + '_refX' + str(reflectionX) + '_rotDeg' + str(rotAngle_deg) +'.' + extensionImages)
+            sitk.WriteImage(atlasSliceManLabelTransformed, outputAugmentedLinearPath + atlasNames[i] + '_refX' + str(reflectionX) + '_rotDeg' + str(rotAngle_deg) + tagManLabels + '.' + extensionImages)
+
+            # Show images:
+            if DEBUG:
+                slice = sitk.GetArrayFromImage(atlasSliceImageTransformed)
+                manLabels = sitk.GetArrayFromImage(atlasSliceManLabelTransformed)
+                plt.subplot(1, 3, 3)  #aca algo tengo que cambiar
+                plt.imshow(slice, cmap='gray')
+                plt.imshow(manLabels, cmap='cold', alpha=0.5)
+                plt.show()
+
+    ################################### AUGMENTATE WITH NONLINEAR TRANSFORMATIONS ########################################
+for i in range(0, len(atlasNames)):
+    # Read target image:
+    atlasSliceImage = sitk.ReadImage(atlasImageFilenames[i])
+    atlasSliceManLabel = sitk.ReadImage(atlasLabelsFilenames[i])
+
+    # Cast the image as float:
+    atlasSliceImage = sitk.Cast(atlasSliceImage, sitk.sitkFloat32)   #lo convierte en float 32
+    for j in range(0, len(atlasNames)):
+        if atlasNames[i] == atlasNames[j]:
+            continue
+        # Image to realign to:
+        fixedSliceImage = sitk.ReadImage(atlasImageFilenames[j])
+        #fixedSliceImage = fixedSliceImage[:, :, 0]
+        ############## NONRIGID REGISTRATION #############
+        # elastixImageFilter filter
+        elastixImageFilter = sitk.ElastixImageFilter()
+        # Parameter maps:
+        parameterMapVector = sitk.VectorOfParameterMap()
+        parameterMapVector.append(elastixImageFilter.ReadParameterFile(parameterFilesPath
+                                                                       + paramFileRigid + '.txt'))
+        parameterMapVector.append(elastixImageFilter.ReadParameterFile(parameterFilesPath
+                                                                       + paramFileNonRigid + '.txt'))
+        # Registration:
+        elastixImageFilter.SetFixedImage(fixedSliceImage)
+        elastixImageFilter.SetMovingImage(atlasSliceImage)
+        elastixImageFilter.SetParameterMap(parameterMapVector)
+        elastixImageFilter.Execute()
+        # Get result and apply transform to labels:
+        # Get the images:
+        atlasSliceImageDeformed = elastixImageFilter.GetResultImage()
+
+        # Apply transform:
+        transformixImageFilter = sitk.TransformixImageFilter()
+        transformixImageFilter.SetTransformParameterMap(elastixImageFilter.GetTransformParameterMap())
+        transformixImageFilter.SetTransformParameter("FinalBSplineInterpolationOrder", "0")
+        transformixImageFilter.SetTransformParameter("ResultImagePixelType", "unsigned char")
+        transformixImageFilter.SetMovingImage(atlasSliceManLabel)
+        transformixImageFilter.Execute()
+        atlasSliceManLabelDeformed = sitk.Cast(transformixImageFilter.GetResultImage(), sitk.sitkUInt8)
+        # write the 2d images:
+        sitk.WriteImage(atlasSliceImageDeformed, outputAugmentedNonLinearPath + atlasNames[i] + '_' + atlasNames[j] + '.' + extensionImages)
+        sitk.WriteImage(atlasSliceManLabelDeformed, outputAugmentedNonLinearPath + atlasNames[i] + '_' + atlasNames[j] + tagManLabels + '.' + extensionImages)
+
+
